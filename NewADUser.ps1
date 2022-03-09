8cb74cd8781f70d102e5967abf429610c5674d77
CeliaMMF <75531637+CeliaMMF@users.noreply.github.com>
2022-03-09 09:31:46 +0100

    Ajout d'un champ "groupe" dans le formulaire et ajout de la commande ADDUSERGROUPE dans le powershell

--- NewADUser.ps1
+++ NewADUser.ps1
@@ -1,5 +1,8 @@
 $login = $args[0]
 $mdp = $args[1]
+$groupe = $args[2]
 $mdp_temp = ConvertTo-SecureString ($mdp) -Force -AsPlainText
-New-ADUser -Name "$login" -SamAccountName "$login" -AccountPassword $mdp_temp -Enabled $true 
+New-ADUser -Name "$login" -SamAccountName "$login" -AccountPassword $mdp_temp -Enabled $true
+Add-ADGroupMember -Identity $groupe -Members $login
 echo "Le login de votre compte est :" $login
+echo "Il appartient au groupe :" $groupe
