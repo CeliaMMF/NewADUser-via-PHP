4b7c115e97b1084b8d76497f64265864280ae90a
CeliaMMF <75531637+CeliaMMF@users.noreply.github.com>
2022-03-10 16:28:41 +0100

    Update NewADUser.ps1

--- NewADUser.ps1
+++ NewADUser.ps1
@@ -1,8 +1,10 @@
 $login = $args[0]
 $mdp = $args[1]
 $groupe = $args[2]
+$nom = $args[3]
+$prenom = $args[4]
 $mdp_temp = ConvertTo-SecureString ($mdp) -Force -AsPlainText
-New-ADUser -Name "$login" -SamAccountName "$login" -AccountPassword $mdp_temp -Enabled $true
+New-ADUser -Name "$nom" -Surname "$prenom" -SamAccountName "$login" -AccountPassword $mdp_temp -Enabled $true
 Add-ADGroupMember -Identity $groupe -Members $login
 echo "Le login de votre compte est :" $login
 echo "Il appartient au groupe :" $groupe
