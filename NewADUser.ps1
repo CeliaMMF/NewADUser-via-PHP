965e9a3b54add55f41ded2f5cbed853cf12b4334
XionMMF <75531637+CeliaMMF@users.noreply.github.com>
2022-03-12 23:40:17 +0100

    ré-activation de la check box & ajout d'un if else dans le powershell

--- NewADUser.ps1
+++ NewADUser.ps1
@@ -3,8 +3,14 @@ $mdp = $args[3]
 $groupe = $args[4]
 $nom = $args[0]
 $prenom = $args[1]
+$mdp_unique = $true
 $mdp_temp = ConvertTo-SecureString ($mdp) -Force -AsPlainText
-New-ADUser -Name "$nom" -Surname "$prenom" -SamAccountName "$login" -AccountPassword $mdp_temp -Enabled $true
+if (!$mdp_unique){
+    New-ADUser -Name "$nom" -Surname "$prenom" -SamAccountName "$login" -AccountPassword $mdp_temp -Enabled $true
+}
+else{
+    New-ADUser -Name "$nom" -Surname "$prenom" -SamAccountName "$login" -AccountPassword $mdp_temp -Enabled $true --ChangePasswordAtLogon $true
+}
 Add-ADGroupMember -Identity $groupe -Members $login
 Write-Output "Le login de votre compte est :" $login
 Write-Output "Il appartient au groupe :" $groupe
