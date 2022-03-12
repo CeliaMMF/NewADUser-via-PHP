a78e82d55c8655e29f1f717c5755b2a776a29a84
XionMMF <75531637+CeliaMMF@users.noreply.github.com>
2022-03-13 00:31:11 +0100

    test du isset

--- reponse1.php
+++ reponse1.php
@@ -8,10 +8,11 @@
         $login = $prenom . "." . $nom ;
         $login = strtolower($login);
         if(isset($_POST["mdptemp"])){
-                
+                $mdp_unique = $false
         }
-        $cmd = "powershell.exe -ExecutionPolicy Bypass -NoProfile -File \"NewADUser.ps1\" $nom $prenom $login $mdp $groupe";
+        // $cmd = "powershell.exe -ExecutionPolicy Bypass -NoProfile -File \"NewADUser.ps1\" $nom $prenom $login $mdp $groupe $mdp_unique";
         // affichage
+        echo $mdp_unique;
         echo "Bonjour  $prenom $nom <br />";
         echo "votre session à bien été créé voici le login $login, il fait parti du groupe $groupe<br />";
         echo "Et voici votre mot de passe $mdp<br />";
