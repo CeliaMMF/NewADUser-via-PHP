27249c12bcf04bd8e0ccc00dc075e30677aa5b55
XionMMF <75531637+CeliaMMF@users.noreply.github.com>
2022-03-13 00:51:24 +0100

    réactivation du script

--- reponse1.php
+++ reponse1.php
@@ -11,9 +11,8 @@
         if(isset($_POST["mdptemp"])){
                 $mdp_unique = "unique";
         }
-        // $cmd = "powershell.exe -ExecutionPolicy Bypass -NoProfile -File \"NewADUser.ps1\" $nom $prenom $login $mdp $groupe $mdp_unique";
+        $cmd = "powershell.exe -ExecutionPolicy Bypass -NoProfile -File \"NewADUser.ps1\" $nom $prenom $login $mdp $groupe $mdp_unique";
         // affichage
-        echo $mdp_unique;
         echo "Bonjour  $prenom $nom <br />";
         echo "votre session à bien été créé voici le login $login, il fait parti du groupe $groupe<br />";
         echo "Et voici votre mot de passe $mdp<br />";
