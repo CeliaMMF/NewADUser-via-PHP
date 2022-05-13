e5ce137fdb4fec157a8ee579afdda414932edf48
CeliaMMF <75531637+CeliaMMF@users.noreply.github.com>
2022-05-13 14:49:43 +0200

    13/05/2022

--- NewADUser.ps1
+++ NewADUser.ps1
@@ -1,21 +1,12 @@
+start-transcript -path ".\logs\log.rtf"
 $login = $args[2]
 $mdp = $args[3]
 $groupe = $args[4]
 $nom = $args[0]
 $prenom = $args[1]
-[bool]$mdp_unique = $args[5]
-$mdp_temp = ConvertTo-SecureString ($mdp) -Force -AsPlainText
-if ($mdp_unique -eq 0){
-    New-ADUser -Name "$nom" -Surname "$prenom" -SamAccountName "$login" -AccountPassword $mdp_temp -Enabled $true
-    $unique = Write-Output "Le mot de passe n'est pas unique"
-}
-elseif($mdp_unique -eq 1) {
-    New-ADUser -Name "$nom" -Surname "$prenom" -SamAccountName "$login" -AccountPassword $mdp_temp -Enabled $true -ChangePasswordAtLogon $true
-    $unique = Write-Output "Le mot de passe est unique"
-}
+New-ADUser -Name "$nom" -Surname "$prenom" -SamAccountName "$login" -AccountPassword $mdp -Enabled $true
 Add-ADGroupMember -Identity $groupe -Members $login
 $log = Write-Output "Le login de votre compte est :" $login
 $grp = Write-Output "Il appartient au groupe :" $groupe
-$test = "Test"
 return $log, $grp, $unique, $test, $mdp_unique
-
+Stop-Transcript
