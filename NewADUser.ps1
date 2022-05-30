d2bc48108c90af45990489d18ffb7e0ad23d0c5b
XionMMF <75531637+CorentinGAL@users.noreply.github.com>
2022-05-30 12:01:29 +0200

    Update NewADUser.ps1

--- NewADUser.ps1
+++ NewADUser.ps1
@@ -1,8 +1,8 @@
+$nom = $args[0]
+$prenom = $args[1]
 $login = $args[2]
 $mdp = $args[3]
 $groupe = $args[4]
-$nom = $args[0]
-$prenom = $args[1]
 New-ADUser -Name "$nom" -Surname "$prenom" -SamAccountName "$login" -AccountPassword $mdp -Enabled $true
 Add-ADGroupMember -Identity $groupe -Members $login
 $log = Write-Output "Le login de votre compte est :" $login
