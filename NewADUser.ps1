1501204a92b017f59bc0bdb310868e7c22c71b5f
XionMMF <75531637+CorentinGAL@users.noreply.github.com>
2022-05-30 12:13:26 +0200

    Update NewADUser.ps1

--- NewADUser.ps1
+++ NewADUser.ps1
@@ -5,7 +5,6 @@ $mdp = $args[3]
 $groupe = $args[4]
 New-ADUser -Name "$nom" -Surname "$prenom" -SamAccountName "$login" -AccountPassword $mdp -Enabled $true
 Add-ADGroupMember -Identity $groupe -Members $login
-$log = Write-Output "Le login de votre compte est :" $login
-$grp = Write-Output "Il appartient au groupe :" $groupe
+$login = Write-Output "Le login de votre compte est :" $login
+$groupe = Write-Output "Il appartient au groupe :" $groupe
 return $log, $grp, $unique, $test, $mdp_unique
-Stop-Transcript
